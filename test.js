8bc16ff5d89f7029c1d8da70433c098b73dbe814
Vloon <pleunscholten@hotmail.com>
2024-06-10 13:14:00 +0200

    remove test file

--- test.js
+++
@@ -1,8 +0,0 @@
-var two_d = [[1,2,3],[4,5,6],[7,8,9]];
-
-// take the third column
-function getColumn(array, index) {
-    return array.map((value) => value[index]);
-}
-var col3 = getColumn(two_d, 1)
-console.log(col3);
