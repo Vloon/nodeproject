9b2fbe7b6b3e7566c53ca375ffa0bb34b14e3c2b
Vloon <pleunscholten@hotmail.com>
2024-06-12 16:20:06 +0200

    Added documentation

--- user.ts
+++ user.ts
@@ -7,6 +7,13 @@ import { twoDimArray } from './matrix';
 export class User {
     ratedNetwork:RatedNetwork;
 
+    /**
+     * Constructor of User class. 
+     * Leaving profile null initializes the user with an all-zero similarity matrix.
+     * Leaving ratings null initializes the user with an all-null ratings vector.
+     * @param profile userProfile that captures the user's node preferences
+     * @param ratings ratings vector that the user has 
+     */
     constructor(profile:UserProfile|null = null, ratings:Rating[]|null = null) {
         if (profile === null) {
             if (ratings === null) throw new AssertionError({message:`If profile is not passed, ratings must be passed`});
@@ -22,13 +29,4 @@ export class User {
         this.ratedNetwork = new RatedNetwork(profile, ratings); 
         // More user-specific information...
     }
-
-    getRatings(): Rating[] {
-        return this.ratedNetwork.ratings;
-    }
-
-    getProfile(): UserProfile {
-        return this.ratedNetwork.profile;
-    }
-
 }
