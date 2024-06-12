5a3a6b4b6bd0712ec6777b3177b8c461c87dfdf2
Vloon <pleunscholten@hotmail.com>
2024-06-12 16:19:26 +0200

    Made randomNetwork & randomRatings public, and added documentation

--- network-generator.ts
+++ network-generator.ts
@@ -1,25 +1,33 @@
 import assert from 'assert';
 import { RatedNetwork } from './rated-network'; 
 import { Rating } from './rating';
+import { twoDimArray } from './matrix';
+import { UserProfile } from './user-profile';
 
 export class NetworkGenerator {
 
     /**
-     * Creates a random rated network.
+     * Creates a random rated network, consisting of an adjacency matrix + a rating for each node.
      * @param nNodes number of nodes in the network
-     * @param lower minimum similarity value of an edge
-     * @param upper maximum similarity value of an edge
+     * @param lower minimum value of an edge
+     * @param upper maximum value of an edge
      * @param ratedProbability probability of a node being rated, allows the network to come with pre-rated nodes
-     * @returns the rated network consisting of an adjacency matrix + a rating for each node.
+     * @returns random rated network
      */
-   static generateRandomNetwork(nNodes:number, lower:number = 0, upper:number = 1, ratedProbability:number=0): RatedNetwork {
+   static randomRatedNetwork(nNodes:number, lower:number = 0, upper:number = 1, ratedProbability:number=0): RatedNetwork {
         assert(0 <= ratedProbability && ratedProbability <= 1, `0 <= ratedProbability <= 1 must hold, but it is ${ratedProbability} instead`);
         let network = this.randomNetwork(nNodes, upper, lower);
         let ratings = this.randomRatings(nNodes, ratedProbability);
-        return new RatedNetwork(network, ratings);
+        return new RatedNetwork(new UserProfile(network), ratings);
     }   
 
-    private static randomRatings(nNodes:number, ratedProbability:number) : Rating[] {
+    /**
+     * Creates a randomly generated list of node ratings
+     * @param nNodes number of nodes in the network
+     * @param ratedProbability probability of rating a node
+     * @returns array of randomly generated node ratings
+     */
+    static randomRatings(nNodes:number, ratedProbability:number) : Rating[] {
         var ratings: Rating[] = [];
         for (let i = 0; i < nNodes; i++) 
             if (Math.random() < ratedProbability) 
@@ -29,10 +37,15 @@ export class NetworkGenerator {
         return ratings;
     }
 
-    private static randomNetwork(nNodes:number, upper:number, lower:number) : number[][] {
-        // Initialize network to be N x N 
-        var network: number[][] = Array<number[]>(nNodes).fill(Array<number>(nNodes).fill(0));
-        // Fill network
+    /**
+     * Generates a random nNodes x nNodes network, with edge values between lower and upper.
+     * @param nNodes number of nodes in the network
+     * @param upper maximum edge value
+     * @param lower minimum edge value
+     * @returns randomly generated network
+     */
+    static randomNetwork(nNodes:number, upper:number, lower:number) : number[][] {
+        var network: number[][] = twoDimArray(nNodes, nNodes, 0);
         for (let i = 0; i < nNodes; i++)
             for (let j = i+1; j < nNodes; j++) {
                 let val = Math.random() * (upper-lower) + lower;
