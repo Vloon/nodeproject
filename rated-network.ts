7f6c3c712318794bd644806cafe27db09c33f474
Vloon <pleunscholten@hotmail.com>
2024-06-12 14:54:02 +0200

    Update rated-network, add documentation, make assertions better

--- rated-network.ts
+++ rated-network.ts
@@ -1,58 +1,45 @@
 import assert from 'assert';
 import { Rating } from './rating';
 import { twoDimReduce } from './matrix';
+import { UserProfile } from './user-profile';
 
 export class RatedNetwork {
     nNodes:number;
-    network:number[][];
+    profile:UserProfile;
     ratings:Rating[];
     lower:number;
     upper:number;
 
-    constructor(network:number[][], ratings:Rating[], lower:number|null=null, upper:number|null=null) {
-        let nRows: number = network.length;
-        let nCols: number[] = [];
-        network.forEach((col) => nCols.push(col.length)); 
-        let sameColLengths = nCols.every((colLength) => colLength === nCols[0]);
-        assert(sameColLengths, `Ragged columns!`);
+    constructor(profile:UserProfile, ratings:Rating[], lower:number|null=null, upper:number|null=null) {
+        let nRows: number = profile.length;
+        let nCols: number[] = profile.similarityNetwork.map((v) => v.length);
+        assert(nCols.every((colLength) => colLength === nCols[0]), `Ragged columns!`);
         assert(nCols[0] === nRows, `Adjacency matrix has invalid shape ${nRows}x${nCols[0]}`);
         assert(ratings.length === nRows, `rating and network must have compatible size, but rating is of length ${ratings.length}, network has shape ${nRows}x${nCols[0]}`);
         this.nNodes = nRows;
-        this.network = network;
+        this.profile = profile;
         this.ratings = ratings;
-        if (lower === null) lower = Math.floor(twoDimReduce(network, (e1, e2) => e1 < e2));
-        if (upper === null) upper = Math.ceil(twoDimReduce(network, (e1, e2) => e1 > e2));
-        assert (lower < upper, `lower must be smaller than upper but they are ${lower} and ${upper}`);
+        if (lower === null) lower = Math.floor(twoDimReduce(profile.similarityNetwork, (e1, e2) => e1 < e2));
+        if (upper === null) upper = Math.ceil(twoDimReduce(profile.similarityNetwork, (e1, e2) => e1 > e2));
+        assert (lower <= upper, `lower must be smaller than upper but they are ${lower} and ${upper}`);
         this.lower = lower;
         this.upper = upper;
     }
 
+    /**
+     * 
+     * @returns Returns whether any node has been rated.
+     */
     hasRating(): boolean {
         return !this.ratings.every((r) => r === null);
     }
 
+    /**
+     * 
+     * @returns Returns whether any node has not yet been rated.
+     */
     hasUnrated(): boolean {
         return !this.ratings.every((r) => r !== null);
-    }
-
-    toString() : string[][] {
-        var visNetwork: string[][] = Array(this.nNodes+1);
-        var ratingsString: string[] = [];
-        this.ratings.forEach((rating) => {
-            ratingsString.push((rating === null) ? '-' : `${rating.toString()} stars`);
-        });
-        ratingsString.unshift('.');
-        // Fill "border" with ratings
-        for (let i = 0; i < this.nNodes+1; i++) {
-            visNetwork[i] = Array(this.nNodes+1);
-            visNetwork[i][0] = ratingsString[i];
-            visNetwork[0][i] = ratingsString[i];
-        }
-        // Fill adjacency middle
-        for (let i = 0; i < this.nNodes; i++) 
-            for (let j = 0; j < this.nNodes; j++)
-                visNetwork[i+1][j+1] = this.network[i][j].toString();
-        return visNetwork;
     }
 
     /**
@@ -63,5 +50,29 @@ export class RatedNetwork {
     addRating(nodeIndex:number, rating:Rating) : void {
         assert(nodeIndex >= 0 && nodeIndex < this.nNodes, `nodeIndex must be between 0 and ${this.nNodes}, but is ${nodeIndex}`);
         this.ratings[nodeIndex] = rating;
+    }
+
+    /**
+     * 
+     * @returns A string array representation appropriate for printing using `console.table`.
+     */
+    toString() : string[][] {
+        var visNetwork: string[][] = new Array(this.nNodes+1);
+        var ratingsString: string[] = [];
+        this.ratings.forEach((rating) => {
+            ratingsString.push((rating === null) ? '-' : `${rating.toString()} stars`);
+        });
+        ratingsString.unshift('.');
+        // Fill "border" with ratings
+        for (let i = 0; i < this.nNodes+1; i++) {
+            visNetwork[i] = new Array(this.nNodes+1);
+            visNetwork[i][0] = ratingsString[i];
+            visNetwork[0][i] = ratingsString[i];
+        }
+        // Fill adjacency middle
+        for (let i = 0; i < this.nNodes; i++) 
+            for (let j = 0; j < this.nNodes; j++)
+                visNetwork[i+1][j+1] = this.profile.similarityNetwork[i][j].toString();
+        return visNetwork;
     }
 }
